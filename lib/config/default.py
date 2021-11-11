e5e12a64fb21d50ce993fc1d245813cac7ca05ac
Reza Baharani <mbaharan@uncc.edu>
2021-11-10 20:31:13 -0500

    Can load the eff_hr_net now.

--- lib/config/default.py
+++ lib/config/default.py
@@ -158,7 +158,7 @@ _C.DEBUG.SAVE_TAGMAPS_PRED = True
 
 def update_config(cfg, args):
     cfg.defrost()
-    cfg.merge_from_file(args.cfg)
+    cfg.merge_from_file(args.hrnet_cfg)
     cfg.merge_from_list(args.opts)
 
     if not os.path.exists(cfg.DATASET.ROOT):
